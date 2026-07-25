816f1f3f1b44584746444267b56dba4baf4fd476
Swarup <swarupshakya63@gmail.com>
2026-07-25 19:29:03 +0545

    Add React Router Navigation Setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,35 @@ import Trends from './components/TrendJobs'
 import Statistics from './components/Statistics'
 import CommunityFeed from './components/CommunityFeed'
 import AuthPage from './components/AuthPage'
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Landing />
-      <Trends />
-      <Statistics />
-      <CommunityFeed />
-      <AuthPage />
+    
+      <div>
+        <Navbar />
+    <Routes>
+      <Route path="/"
+      element={
+        <>
+          <Landing />
+          <Trends />
+          <Statistics />
+          <CommunityFeed />
+        </>
+      }
+      />
+
+      <Route path="/auth" element={
+        <>
+          <AuthPage />
+        </>
+      }
+
+      />
+        
+      
+    </Routes>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+  
   return (
     <nav className="h-20 bg-white text-black flex items-center justify-between px-5 md:px-10 lg:px-16 shadow-sm transition-all duration-300">
 
       {/* Logo */}
       <div className="text-2xl md:text-3xl font-bold tracking-tight animate-fadeIn">
-        <h1>
+        <h1 onClick={()=>{navigate("/")}}>
           Skill<span className="text-green-700">Gig:</span>
         </h1>
       </div>
@@ -17,14 +21,33 @@ const Navbar = () => {
 
         <ul className="flex items-center gap-7">
 
-          {["Overview", "Home", "Cover", "Statistics", "Contact"].map(
+          {[{
+            'name': "Overview"
+          },
+            {
+              'name': "Home",
+              'path': '/',
+            },
+            {
+              'name':"Cover"
+            },
+            {
+              'name':"Statistics"
+            },
+            {
+              'name':"Contact"
+            }]
+          .map(
             (item) => (
-              <li key={item}>
+              <li key={item.name}>
                 <a
+                onClick={(e)=>{ e.preventDefault();
+                   navigate(item.path)}}
+                
                   href="#"
                   className="relative transition-colors duration-300 hover:text-green-700 group"
                 >
-                  {item}
+                  {item.name}
 
                   <span className="absolute left-0 bottom-[-6px] w-0 h-[2px] bg-green-700 transition-all duration-300 group-hover:w-full"></span>
 
@@ -39,12 +62,12 @@ const Navbar = () => {
         {/* Buttons */}
         <div className="flex items-center gap-4">
 
-          <button className="cursor-pointer transition-all duration-300 hover:text-green-700 hover:-translate-y-0.5">
+          <button className="cursor-pointer transition-all duration-300 hover:text-green-700 hover:-translate-y-0.5" onClick={()=>{navigate("/auth")}}>
             Login
           </button>
 
 
-          <button className="rounded-full px-6 py-2.5 bg-green-700 text-white shadow-md transition-all duration-300 hover:bg-green-800 hover:shadow-lg hover:-translate-y-1 active:scale-95">
+          <button onClick={()=>{navigate("/auth")}} className="rounded-full px-6 py-2.5 bg-green-700 text-white shadow-md transition-all duration-300 hover:bg-green-800 hover:shadow-lg hover:-translate-y-1 active:scale-95">
 
             SignUp
 
